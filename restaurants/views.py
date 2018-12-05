d46879686559cc710864867964a86d0ae0d79153
aishabn <abn.226@gmail.com>
2018-12-05 19:33:09 +0300

    task_10

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Restaurant
-from .forms import RestaurantForm, SignupForm, SigninForm
+from .models import Restaurant, Item
+from .forms import RestaurantForm, SignupForm, SigninForm, ItemForm
 from django.contrib.auth import login, authenticate, logout
 
 def signup(request):
@@ -51,7 +51,8 @@ def restaurant_list(request):
 
 def restaurant_detail(request, restaurant_id):
     context = {
-        "restaurant": Restaurant.objects.get(id=restaurant_id)
+        "restaurant": Restaurant.objects.get(id=restaurant_id),
+        "items": Item.objects.filter(restaurant=restaurant_id)
     }
     return render(request, 'detail.html', context)
 
@@ -60,16 +61,30 @@ def restaurant_create(request):
     if request.method == "POST":
         form = RestaurantForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            rest_obj = form.save(commit=False)
+            rest_obj.owner = request.user
+            rest_obj.save()
             return redirect('restaurant-list')
     context = {
         "form":form,
+
     }
     return render(request, 'create.html', context)
 
-def item_create(request):
+def item_create(request, restaurant_id):
+    rest_obj = Restaurant.objects.get(id=restaurant_id)
+    form = ItemForm()
+    if request.method == "POST":
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            item_obj = form.save(commit=False)
+            item_obj.restaurant = rest_obj
+            item_obj.save()
+            return redirect ('restaurant-detail', restaurant_id )
 
-    context = {
+    context = { 
+        "item": form,
+        "rest_obj" : rest_obj,
         
     }
     return render(request, 'item_create.html', context)
